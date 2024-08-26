429a6f33edc1fbc72494f65e7265432c96c4f4ae
VitoriaDelaciPereira <164279805+VitoriaDelaciPereira@users.noreply.github.com>
2024-08-26 23:04:21 +0000

    26/08/2024

--- index.js
+++ index.js
@@ -11,3 +11,22 @@ console.log(anoAtual);
 
 idade = idade + 1;
 console.log(idade);
+
+//Agência de viagens Hawaii Paris Nova York Rio de Janeiro Tóquio
+const listaDeDestinos = new Array(
+  "Hawaii",
+  "Paris",
+  "Nova York",
+  "Rio de Janeiro",
+  "Tóquio"
+);
+
+console.log(listaDeDestinos);
+console.log(listaDeDestinos[3]);
+listaDeDestinos.push("Coronel vivida");
+listaDeDestinos.push("Curitiba")
+listaDeDestinos.push("Auracária");
+listaDeDestinos.push("Barcelona");
+console.log(listaDeDestinos);
+
+
