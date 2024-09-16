a9e08b59b409374278bcb9d92ab3538a9a2d5758
VitoriaDelaciPereira <164279805+VitoriaDelaciPereira@users.noreply.github.com>
2024-09-16 23:06:38 +0000

    16-09-2024

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const prompt = require('prompt-sync');
+const entrada = prompt();
+
 let idade = 17;
 const anoNascimento = 2006;
 let anoAtual = 2024;
@@ -29,12 +32,15 @@ listaDeDestinos.push("Auracária");
 listaDeDestinos.push("Barcelona");
 console.log(listaDeDestinos);
 
-const idadeComprador = 17;
+var nomeComprador = entrada("Digite seu nome: ");
+var idadeComprador = entrada("Qual a sua Idade?");
 
 if(idadeComprador >= 18) {
+  console.log("Olá senhor(a) " + nomeComprador);
   console.log("Comprador maior de idade");
   console.log("destinos disponíveis: " + listaDeDestinos);
 } else {
+  console.log("Olá senhor(a) " + nomeComprador);
   console.log("Não foi possível consluir a compra");
   console.log("Você é menor de idade");
 }
