ae3af3eadaaf7d01feda87535426c2ec2d0a0946
VitoriaDelaciPereira <164279805+VitoriaDelaciPereira@users.noreply.github.com>
2024-09-23 22:58:57 +0000

    26/09/2024

--- index.js
+++ index.js
@@ -38,7 +38,16 @@ var idadeComprador = entrada("Qual a sua Idade?");
 if(idadeComprador >= 18) {
   console.log("Olá senhor(a) " + nomeComprador);
   console.log("Comprador maior de idade");
-  console.log("destinos disponíveis: " + listaDeDestinos);
+  console.log("Escolha seu destino")
+  
+  var contador = 0;
+
+  while(contador <9){
+    console.log(listaDeDestinos[contador]);
+    contador++;
+  }
+  var destino = entrada("Digite o número que corresponde ao destino selecionado (0-8)");
+  console.log(`destino selecionado foi: ${listaDeDestinos[destino]}`);
 } else {
   console.log("Olá senhor(a) " + nomeComprador);
   console.log("Não foi possível consluir a compra");
